c44a1b7bd0480be5f3728ff6ed7080ad678ee090
Ketan Arora <ketan.arora019@gmail.com>
2026-01-13 21:05:51 +0530

    Updated backend link

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.CONFIG = {
     // 🔴 Production backend URL (Render deployment)
-    BASE_URL: 'https://gruhini2.onrender.com',
+    BASE_URL: 'https://gruhinibackend.onrender.com',
 
     // 🔴 CHANGE THIS to your Razorpay Key ID
     RAZORPAY_KEY: "rzp_test_YOUR_KEY_HERE",
