2a7f4d9f23983b348b95227ac2762ae0a622d2fa
unknown <vanshika21rajput@gmail.com>
2026-03-19 17:25:01 +0530

    order_and_admin controls completed

---
+++ src/main/java/com/example/Gruhani/service/OrderService.java
@@ -0,0 +1,310 @@
+package com.example.Gruhani.service;
+
+import com.example.Gruhani.Enums.OrderStatus;
+import com.example.Gruhani.Package.*;
+import com.example.Gruhani.Repositories.OrderRepository;
+import com.example.Gruhani.Repositories.ProductRepo;
+import com.example.Gruhani.Repositories.SellerRepo;
+import com.example.Gruhani.Repositories.UserRepo;
+import com.example.Gruhani.dtos.*;
+import com.example.Gruhani.models.OrderItem;
+
+import com.example.Gruhani.models.*;
+import com.example.Gruhani.models.Users;
+import jakarta.persistence.OptimisticLockException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Service
+public class orderService {
+
+    @Autowired
+    UserRepo userRepo;
+    @Autowired
+    ProductRepo productRepo;
+    @Autowired
+    OrderRepository orderRepository;
+    @Autowired
+    usernameFromContext usernameFromContext;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    SellerRepo sellerRepo;
+
+    public List<OrderItem> MaptoOrderItem(List<CartItem> cartItemList, Order order) {
+        List<OrderItem> orderItemList = new ArrayList<>();
+        for (CartItem i : cartItemList) {
+            OrderItem orderItem = new OrderItem();
+
+            orderItem.setProduct(i.getProduct());
+            orderItem.setQuantity(i.getQuantity());
+            orderItem.setPriceAtOrderTime(i.getPriceAtAddTime());
+            orderItem.setOrder(order);
+            orderItemList.add(orderItem);
+        }
+        return orderItemList;
+
+    }
+
+
+    @Transactional
+    public OrderSellerResponseDto processOrder(orderReceiveDto receiveDto, HttpServletRequest req) {
+        String username = usernameFromContext.fetchUsername();
+        Users user = userRepo.findByemail(username).orElseThrow(() -> new RuntimeException("USER NOT FOUND"));
+        Cart cart = user.getCart();
+        if (cart == null) {
+            throw new InvalidCart("Cart Doesn't Exist");
+        }
+        Order order = new Order();
+        List<OrderItem> list = MaptoOrderItem(cart.getCartItems(), order);
+        if (list.isEmpty()) {
+            throw new IllegalArgumentException("Cart cannot be empty");
+        }
+        order.setUser(user);
+        order.setDeliveryTime("3-4 Days");
+        order.setDeliveryAddress(receiveDto.getAddress());
+        int otp = ThreadLocalRandom.current().nextInt(100000, 1_000_000);
+        String hashedOtp = bCryptPasswordEncoder.encode(String.valueOf(otp));
+        order.setHashedOtp(hashedOtp);
+        order.setExpiration(LocalDateTime.now().plusDays(5));
+        order.setOtpVerified(false);
+        order.setOrderItemList(list);
+        order.setOrderStatus(OrderStatus.PENDING);
+        order.setPlacedAt(LocalDateTime.now());//we could have used @prepersist on field itself
+        order.setOrderValue(calculateOrderValue(list));
+        try {
+            validateAndDecreaseStock(order.getOrderItemList());
+        } catch (OptimisticLockException exception) {
+            throw new OptimisticLockException("Other user has updated this product's stock! Kindly Retry");
+        }
+        order.setSeller(list.get(0).getProduct().getSeller());
+
+        order.setMessage("Order placed Successfully");
+        orderRepository.save(order);
+        cart.getCartItems().clear();
+        //Setting seller details to send to user
+        SellerDetailsDto sellerDetailsDto = new SellerDetailsDto();
+        sellerDetailsDto.setAddress(order.getSeller().getAddress());
+        sellerDetailsDto.setName(order.getSeller().getUser().getName());
+        sellerDetailsDto.setContact(order.getSeller().getContactNo());
+        sellerDetailsDto.setBusinessName(order.getSeller().getBusinessName());
+
+        List<OrderItemDto> orderItemDtos = OrderItemtoDto(list);
+
+        return new OrderSellerResponseDto(order.getId(), order.getOrderValue(), order.getPlacedAt(), order.getMessage(), sellerDetailsDto, OrderStatus.PENDING, order.getDeliveryTime(), order.getDeliveryAddress(), orderItemDtos);
+//frotend must show placed order and pending both
+
+    }
+
+    private List<OrderItemDto> OrderItemtoDto(List<OrderItem> list) {
+        List<OrderItemDto> orderItemDtos = new ArrayList<>();
+        for (OrderItem orderItem : list) {
+            OrderItemDto orderItemDto = new OrderItemDto();
+            orderItemDto.setPriceAtOrderTime(orderItem.getPriceAtOrderTime());
+            orderItemDto.setQuantity(orderItem.getQuantity());
+            orderItemDto.setProductImage(orderItem.getProduct().getImage());
+            orderItemDto.setProductName(orderItem.getProduct().getName());
+            orderItemDtos.add(orderItemDto);
+
+        }
+        return orderItemDtos;
+    }
+
+    @Transactional
+    public void validateAndDecreaseStock(List<OrderItem> list) {
+        for (OrderItem orderItem : list) {
+            Product p = productRepo.findById(orderItem.getProduct().getId())
+                    .orElseThrow(() -> new UserNotFoundException("Product not found"));
+            if (orderItem.getQuantity() > p.getStock()) {
+                throw new StockNotAvailable("Insufficient stock for: " + p.getName());
+            }
+            p.setStock(p.getStock() - orderItem.getQuantity());
+            productRepo.save(p);
+        }
+    }
+
+    @Transactional
+    public void validateAndIncreaseStock(List<OrderItem> list) {
+        for (OrderItem orderItem : list) {
+            Product p = productRepo.findById(orderItem.getProduct().getId())
+                    .orElseThrow(() -> new ProductNotFoundException("Product not found"));
+
+            p.setStock(p.getStock() + orderItem.getQuantity());
+
+        }
+    }
+
+
+    private BigDecimal calculateOrderValue(List<OrderItem> cartItemList) {
+        BigDecimal total = BigDecimal.valueOf(0);
+
+        for (OrderItem i : cartItemList) {
+            BigDecimal price = i.getPriceAtOrderTime();
+            int quantity = i.getQuantity();
+            BigDecimal totalOfAnItem = price.multiply(BigDecimal.valueOf(quantity));
+            total = total.add(totalOfAnItem);
+        }
+        return total;
+    }
+
+    @Transactional
+    public void cancelOrder(Long id) {
+        String username = usernameFromContext.fetchUsername();
+        Users user = userRepo.findByemail(username).orElseThrow(() -> new UserNotFoundException("USER NOT FOUND"));
+        Order order = orderRepository.findById(id).orElseThrow(() -> new InvalidOrder("ORDER NOT FOUND"));
+        Long requestUserId = order.getUser().getId();
+        Long currUserId = user.getId();
+        if (!requestUserId.equals(currUserId)) {
+            throw new RuntimeException("User Not Allowed to cancel this Order");
+        }
+        LocalDateTime placedAt = order.getPlacedAt();
+        LocalDateTime now = LocalDateTime.now();
+
+        long hoursDifference = ChronoUnit.HOURS.between(placedAt, now);
+
+        if (hoursDifference > 36) {
+            throw new RuntimeException("Order cannot be cancelled after 36 hours");
+        }
+        order.setOrderStatus(OrderStatus.CANCELLED);
+        try {
+            validateAndIncreaseStock(order.getOrderItemList());
+        } catch (OptimisticLockException e) {
+            throw new OptimisticLockException("Retry ORDERING");
+        }
+    }
+
+
+    public List<OrderSellerResponseDto >viewOrdersToUser(String orderStatus) {
+        String username = usernameFromContext.fetchUsername();
+        Users users = userRepo.findByemail(username)
+                .orElseThrow(() -> new UserNotFoundException("USER NOT FOUND"));
+        List<Order> orders;
+        if (orderStatus==null) {
+            orders = orderRepository.findByUserId(users.getId());
+        } else {
+            try {
+                orders = orderRepository.findByUserIdAndOrderStatus(
+                        users.getId(),
+                        OrderStatus.valueOf(orderStatus.toUpperCase()));
+            } catch (IllegalArgumentException e) {
+                throw new InvalidOrder("Invalid status: " + orderStatus);
+            }
+        }
+        List<OrderSellerResponseDto> response = mapToDto(orders);
+        return response;
+    }
+
+    private List<OrderSellerResponseDto> mapToDto(List<Order> orders) {
+        List<OrderSellerResponseDto> dtoList = new ArrayList<>();
+        for (Order order : orders) {
+
+            // Map OrderItems
+            List<OrderItemDto> orderItemDtos = new ArrayList<>();
+            for (OrderItem item : order.getOrderItemList()) {
+                OrderItemDto itemDto = new OrderItemDto();
+                itemDto.setProductName(item.getProduct().getName());
+                itemDto.setQuantity(item.getQuantity());
+                itemDto.setPriceAtOrderTime(item.getPriceAtOrderTime());
+
+                orderItemDtos.add(itemDto);
+            }
+
+            // Map Seller Details
+            SellerDetailsDto sellerDetailsDto = new SellerDetailsDto();
+            sellerDetailsDto.setName(order.getSeller().getUser().getName());
+            sellerDetailsDto.setBusinessName(order.getSeller().getBusinessName());
+            sellerDetailsDto.setContact(order.getSeller().getContactNo());
+            sellerDetailsDto.setAddress(order.getSeller().getAddress());
+
+            // Build Response DTO
+            OrderSellerResponseDto dto = new OrderSellerResponseDto(
+                    order.getId(),
+                    order.getOrderValue(),
+                    order.getPlacedAt(),
+                    order.getMessage(),
+                    sellerDetailsDto,
+                    order.getOrderStatus(),
+                    order.getDeliveryTime(),
+                    order.getDeliveryAddress(),
+                    orderItemDtos
+            );
+            dtoList.add(dto);
+        }
+        return dtoList;
+    }
+
+    public  List<OrderUserResponseDto>viewOrderToSeller(String orderStatus)
+    {
+        String username=usernameFromContext.fetchUsername();
+        Users users=userRepo.findByemail(username).orElseThrow(()->new UserNotFoundException("USER NOT FOUND"));
+        List<Order> orders=new ArrayList<>();
+        Seller seller = sellerRepo.findByuser_email(username);
+        if (seller == null) {
+            throw new UserNotFoundException("Seller not found");
+        }
+        if(orderStatus==null)
+        {
+            orders=  orderRepository.findBySellerId(seller.getId());
+        }
+        else {
+            try {
+                orders = orderRepository.findBySellerIdAndOrderStatus(
+                        seller.getId(),
+                        OrderStatus.valueOf(orderStatus.toUpperCase()));
+            } catch (IllegalArgumentException e) {
+                throw new InvalidOrder("Invalid status: " + orderStatus);
+            }
+
+        }
+        List<OrderUserResponseDto>orderUserResponses=touserResponseDto(orders);
+         return orderUserResponses;
+
+    }
+
+    private List<OrderUserResponseDto> touserResponseDto(List<Order> orders) {
+        List<OrderUserResponseDto> dtoList = new ArrayList<>();
+        for (Order order : orders) {
+
+            // Map OrderItems
+            List<OrderItemDto> orderItemDtos = new ArrayList<>();
+            for (OrderItem item : order.getOrderItemList()) {
+                OrderItemDto itemDto = new OrderItemDto();
+                itemDto.setProductName(item.getProduct().getName());
+                itemDto.setQuantity(item.getQuantity());
+                itemDto.setPriceAtOrderTime(item.getPriceAtOrderTime());
+
+                orderItemDtos.add(itemDto);
+            }
+
+            // Map Seller Details
+            UserDetailsDto userDetailsDto = new UserDetailsDto();
+            userDetailsDto.setName(order.getUser().getName());
+            userDetailsDto.setEmail(order.getUser().getEmail());
+            userDetailsDto.setContact(order.getUser().getContact());
+            // Build Response DTO
+            OrderUserResponseDto dto = new OrderUserResponseDto(
+                    order.getId(),
+                    order.getOrderValue(),
+                    order.getPlacedAt(),
+                    order.getMessage(),
+                    userDetailsDto,
+                    order.getOrderStatus(),
+                    order.getDeliveryTime(),
+                    order.getDeliveryAddress(),
+                    orderItemDtos
+            );
+            dtoList.add(dto);
+        }
+        return dtoList;
+
+    }
+}
