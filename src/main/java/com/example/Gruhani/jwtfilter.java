ae9a1067bf46fecba6737b17a2408bb18edc1271
unknown <vanshika21rajput@gmail.com>
2026-03-30 23:27:34 +0530

    escaped /forgot-pass from filter chain

--- src/main/java/com/example/Gruhani/jwtfilter.java
+++ src/main/java/com/example/Gruhani/jwtfilter.java
@@ -57,7 +57,7 @@ public class jwtfilter extends OncePerRequestFilter {
         String path = request.getServletPath();
         if (path.equals("/logins") ||
                 path.equals("/register") ||
-                path.equals("/register-seller")||path.equals("/explore")||path.equals("/forgot-password")||path.equals("//verify-otp-forgetPassword")) {
+                path.equals("/register-seller")||path.equals("/explore")||path.equals("/forgot-password")||path.equals("/verify-otp-forgetPassword")) {
             filterChain.doFilter(request, response);
             return;
         }
